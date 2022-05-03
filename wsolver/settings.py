2454c169fde77628c39fe0e3648e259e5e7e9339
James A. Cox <jacox@jacox.net>
2022-05-03 17:13:43 -0500

    x

--- wsolver/settings.py
+++ wsolver/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'wsolver',
         'USER': 'postgres',
-        'PASSWORD': 'myPassword',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432'
     }
